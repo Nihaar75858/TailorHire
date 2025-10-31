4fdea9fe8b5088f029a1736603ef2393e3420b0b
Nihaar75858 <nihar.adhiep@gmail.com>
2025-10-31 17:11:19 +0530

    Refactored Navbar into App.js with selective display

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -1,20 +1,25 @@
 // App.jsx
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Register from "./pages/Auth/Register";
 import Login from "./pages/Auth/Login";
 import UserDashboard from "./pages/User/Userdashboard";
 import PublicDashBoard from "./pages/Public/PublicDashBoard";
+import Navbar from "./components/Navbar/Navbar";
 import "./App.css";
 
 function App() {
+  const location = useLocation();
+  const noNavbarPaths = ["/login", "/register"];
+  const showNavbar = !noNavbarPaths.includes(location.pathname);
 
   return (
     <>
+      {showNavbar && <Navbar />}
       <Routes>
-        <Route path="/" element={<PublicDashBoard />}/>
-        <Route path="/register" element={<Register />}/>
-        <Route path="/login" element={<Login />}/>
-        <Route path="/userdashboard" element={<UserDashboard />}/>
+        <Route path="/" element={<PublicDashBoard />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/userdashboard" element={<UserDashboard />} />
       </Routes>
     </>
   );
