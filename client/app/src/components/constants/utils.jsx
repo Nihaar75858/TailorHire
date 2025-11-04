69ead75fc63906db7c3f3af6432f16ce1eef7532
Nihaar75858 <nihar.adhiep@gmail.com>
2025-11-04 15:57:04 +0530

    Implemented passing test cases for getNavigationConfig for Navbar

--- client/app/src/components/constants/utils.jsx
+++ client/app/src/components/constants/utils.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+const NavigationConfig = {
+  0: [
+    // Viewer / not logged in
+    {},
+  ],
+  1: [
+    // User
+    { name: "Home", to: "/userdashboard", submenus: null },
+    {
+      name: "Jobs",
+      submenus: [
+        { name: "View Jobs", to: "/viewjobs" },
+        { name: "Review Applications", to: "/revapplications" },
+      ],
+    },
+    {
+      name: "Cover Letter",
+      to: "/coverletter",
+      submenus: null,
+    },
+    { name: "Chat", to: "/chat", submenus: null },
+  ],
+};
 
-export const getNavigationConfig = () => [
-  { name: 'Products', to: '#' },
-  { name: 'About', to: '/about' },
-]
+export const getNavigationConfig = (userType) => {
+  return NavigationConfig[userType] || NavigationConfig[0];
+};
